28fc61abb7a68ae55ed57bf83f75404097c83c20
ebin-sabu <ebin.pereppadan01@gmail.com>
2023-09-24 22:31:04 +0100

    Frontend Made and Integrated to Backend

--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import autoAnimate from "@formkit/auto-animate";
+import { useEffect } from "react";
 
-const ChatBody = () => {
+
+const ChatBody = ({chat}) => {
     const aiStyle =
     "bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto";
 
+    const parent = useRef(null);
+    const bottomRef = useRef(null);
+
+    // animations
+    useEffect(()=>{
+        parent.current && autoAnimate(parent.current);
+    }, [parent])
+
+    //Scrolling
+    useEffect(()=>{
+        bottomRef.current?.scrollIntoView({behavior: "smooth"})
+    }, [chat])
+
   return (
     <div className="flex flex-col gap-4">
-
         {/* User Message */}
-        <div className="border-[#797979] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]">
-            <pre className="whitespace-pre-wrap">
-                <span> YO HI OPEN-AI</span>
-            </pre>
-        </div>
-
-        {/* AI - Response */}
-        <div className={`border-[#797979] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%] ${aiStyle}`}>
-            <pre>
-                <span> Hi User, Nice to meet you.</span>
-            </pre>
-        </div>
+        {
+            chat.map((message, i) => {
+                return(
+                    <div key={i} className={`border-[#797979] break-words border-2 rounded-xl self-end px-4 py-3 max-w-[80%]${
+                        message.sender === "ai" && aiStyle }`}>
+                    <pre className="whitespace-pre-wrap">
+                        <span>{message.message}</span>
+                    </pre>
+                    </div>
+                )
+            })
+        }
+        <div ref={bottomRef} className="h-3"></div>
     </div>
   )
 }
--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-const ChatInput = () => {
+const ChatInput = ({sendMessage, loading}) => {
+
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value === "") return;
+    sendMessage({ sender: "user", message: value });
+    setValue("");
+  };
+
   return (
     <div
       className="w-full bg-white bg-opacity-10 max-h-40 rounded-lg px-4
     py-4 overflow-auto relative"
-    >
+            
+            >
+              {loading ? (
+              <img src="./loader.gif" className="w-8 m-auto" />
+            ) : (
+              <>
         
-            <textarea className="border-0 bg-transparent outline-none w-11/12" />
+            <textarea
+              onKeyDown={(e) => {
+                e.keyCode === 13 && e.shiftKey === false && handleSubmit();
+              }} 
+              rows={1}
+              className="border-0 bg-transparent outline-none w-11/12"
+              value={value}
+              type="text"
+              onChange={(e) => setValue(e.target.value)}
+            />
 
             <img
-            src="./send.png"
-            width={20}
-            alt="send-button"
-            className="absolute top-4
-            right-3 hover:cursor-pointer ease-in duration-100 hover:scale-125
-             "
+              onClick={handleSubmit}
+              src="./send.png"
+              width={20}
+              alt="send-button"
+              className="absolute top-4 right-3 hover:cursor-pointer ease-in duration-100 hover:scale-125"
             />
-        
+          </>
+        )}
     </div>
   )
 }
